01014fcb5712aedf2cf0763cb43b79412fdd04d6
srivathsaacharya <srivathsaacharya@gmail.com>
2018-03-03 08:46:25 +0530

    Changes done as per the Requirement. Event is driven by 2 View Sources 1. "Ask" Button 2. Image View by itself.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,53 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnAsk;
+    private ImageView imgResult;
+    private Random mRandom;
+
+    private int resourcemx[] = {
+            R.drawable.ball1,
+            R.drawable.ball2,
+            R.drawable.ball3,
+            R.drawable.ball4,
+            R.drawable.ball5
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialise all components here
+        btnAsk = findViewById(R.id.btnAsk);
+        imgResult = findViewById(R.id.imgResult);
+        mRandom = new Random();
+
+        btnAsk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getResult();
+            }
+        });
+
+        imgResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getResult();
+            }
+        });
+
+    }
+
+    private void getResult()
+    {
+        imgResult.setImageResource(resourcemx[mRandom.nextInt(5)]);
     }
 }
